bf82c4718727f93643bdef914a1dcae987f979d5
Sravani <tangallapallyshravani@gmail.com>
2025-07-26 20:39:27 +0530

    Initial commit

---
+++ e-comm web app/frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,62 @@
+ import React, { useContext, useState } from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
+import { backend_url, currency } from "../../App";
+
+const ProductDisplay = ({ product }) => {
+  const { addToCart } = useContext(ShopContext);
+  const [selectedSize, setSelectedSize] = useState("M");
+
+  return (
+    <div className="productdisplay">
+      <div className="productdisplay-left">
+        <div className="productdisplay-img">
+          <img className="productdisplay-main-img" src={backend_url + product.image} alt="img" />
+        </div>
+      </div>
+
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-stars">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">{currency}{product.old_price}</div>
+          <div className="productdisplay-right-price-new">{currency}{product.new_price}</div>
+        </div>
+        <div className="productdisplay-right-description">
+          {product.description}
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            {["S", "M", "L", "XL", "XXL"].map(size => (
+              <div
+                key={size}
+                className={`size-box ${selectedSize === size ? "selected-size" : ""}`}
+
+                onClick={() => setSelectedSize(size)}
+              >
+                {size}
+              </div>
+            ))}
+          </div>
+        </div>
+        <button onClick={() => addToCart(product.id, selectedSize)}>ADD TO CART</button>
+
+
+        <p className="productdisplay-right-category"><span>Category :</span> {product.category}</p>
+        <p className="productdisplay-right-category"><span>Tags :</span> Modern, Latest</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDisplay;
